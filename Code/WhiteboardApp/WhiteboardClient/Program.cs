b2bc784d32dbde526b6adf480b687b3a03cf4810
Nguyễn Anh Duy <duyna7232@ut.edu.vn>
2026-06-22 13:40:36 +0700

    Update Program.cs

--- Code/WhiteboardApp/WhiteboardClient/Program.cs
+++ Code/WhiteboardApp/WhiteboardClient/Program.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Windows.Forms;
-using System.Threading.Tasks;
-
 namespace WhiteboardClient
 {
     internal static class Program
@@ -9,16 +5,9 @@ namespace WhiteboardClient
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
 
-            // Khởi chạy luồng mạng ngầm song song với giao diện Form1
-            Task.Run(async () => {
-                await NetworkClient.StartClientAsync();
-            });
-
-            // Mở giao diện bảng vẽ
-            Application.Run(new Form1());
+            Application.Run(new FormRoomSelection());
         }
     }
 }
